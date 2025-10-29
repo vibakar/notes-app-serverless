40a57d690be1d329e392830e488060322913c59d
vibakar <vibakar23@gmail.com>
2025-10-29 19:35:30 +0000

    Fix for aws deploy resource stage in infra pipeline

--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -7,13 +7,3 @@ output "frontend_app_url" {
   description = "Frontend application url"
   value       = aws_route53_record.www_frontend.name
 }
-
-output "backend_api_url" {
-  description = "Backend application url"
-  value       = aws_route53_record.backend.name
-}
-
-output "api_gateway_url" {
-  description = "Invoke URL for the Notes API"
-  value       = "${aws_apigatewayv2_api.notes_api.api_endpoint}/${aws_apigatewayv2_stage.notes_stage.name}"
-}
